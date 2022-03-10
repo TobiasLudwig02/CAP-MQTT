7e3ff2f0cfa41127ccedc2cfa4e993912ca1d695
Tobias.Ludwig02 <tobias.ludwig@freudenberg.com>
2022-03-10 11:03:20 +0100

    Fertig

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // See tutorial for more information on which values to use here
     credStoreNamespace: "cap-mqtt",
     targetUrl: "https://cit-iot-sandbox-tmy1xblv.it-cpi018-rt.cfapps.eu10-003.hana.ondemand.com/http/cap-mqtt",
     targetTokenUrl: "https://cit-iot-sandbox-tmy1xblv.authentication.eu10.hana.ondemand.com/oauth/token"
