27708f6077445cc6b88333d33bf4063bdcc99a32
Tobias.Ludwig02 <tobias.ludwig@freudenberg.com>
2022-03-10 08:59:44 +0100

    commit

---
+++ creds-local.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "integration-suite":{
+        username: "sb-bd6bf3d0-93f7-4eaa-b232-837165a51a5e!b127222|it-rt-cit-iot-sandbox-tmy1xblv!b117912",
+        password: "bb022be1-d6cc-42a6-bef0-8e711e748c14$uOJir5izT5AcVppsEPi4Wf0ViYtR-lFZat24zA4MJaA="
+    }
+}
