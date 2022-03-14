cc8c2471680dd5a949bcd22e5bd20213bb3eac63
Tobias.Ludwig02 <tobias.ludwig@freudenberg.com>
2022-03-14 10:31:44 +0100

    Try

--- config.js
+++ config.js
@@ -3,3 +3,4 @@ module.exports = {
     targetUrl: "https://cit-iot-sandbox-tmy1xblv.it-cpi018-rt.cfapps.eu10-003.hana.ondemand.com/http/cap-mqtt",
     targetTokenUrl: "https://cit-iot-sandbox-tmy1xblv.authentication.eu10.hana.ondemand.com/oauth/token"
 }
+//Test
